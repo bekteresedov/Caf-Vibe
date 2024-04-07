cb6bbcb396bb9b8937ad1e2f9b0085b74225c055
Bekter <besedov347@gmail.com>
2024-04-07 14:03:02 +0300

    admin permasion added

--- server/src/app/reservation/reservation.router.js
+++ server/src/app/reservation/reservation.router.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { reservationValidationSchemaMiddleware } from "./validation/reservation.middleware.js";
 import { ReservationController } from "./reservation.controller.js";
+import { isAdmin } from "../../core/middleware/auth-middleware.js";
 const reservationRouter = express.Router();
 const mainPath = "/reservation";
 
@@ -12,11 +13,13 @@ reservationRouter.post(
 
 reservationRouter.get(
   `${mainPath}/get/:id`,
+  isAdmin,
   ReservationController.findReservationById
 );
-reservationRouter.get(`${mainPath}/all`, ReservationController.all);
+reservationRouter.get(`${mainPath}/all`, isAdmin, ReservationController.all);
 reservationRouter.delete(
   `${mainPath}/delete/:id`,
+  isAdmin,
   ReservationController.delete
 );
 
--- server/src/core/middleware/auth-middleware.js
+++ server/src/core/middleware/auth-middleware.js
@@ -1,1 +1,11 @@
-const isAdmin = (request, response, next) => {};
+import { APIError } from "../../shared/dto/error-response.js";
+import { decodedToken } from "../utils/auth/decoded-token.js";
+
+export const isAdmin = async (request, response, next) => {
+  const deToken = await decodedToken(request.cookies.accessToken);
+  if (deToken.role == "admin") {
+    next();
+  } else {
+    throw new APIError("Unauthorized - Admin access required", 403);
+  }
+};
